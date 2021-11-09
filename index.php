f7eddc8e8a02f7e27bcf6e04d9c61195c5f990a4
cmoynard <cmoynard.etude@gmail.com>
2021-11-09 17:43:43 +0100

    index + bug entete session TEST NON

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
   include ('include/entete.inc.php');
 ?>
-
-      <p>Test lourd</p>
-
-        <button type="button" class="btn btn-lg btn-outline-dark">Foto</button>
+<div class="container text-center">
+<div class="py-5 text-center">
+    <img class="d-block mx-auto mb-2" src="images/logo.png" alt="logo photoforyou" width="170" height="115">
+  	<h1 class="display-5">PhotoForYou</h1>
+    <p class="lead">Des pros au service des professionnels de la communication.</p>
+  </div>
+        <?php
+          if ($_SESSION['login']=true AND $_SESSION['type']=='client') {
+           echo '<button type="button" class="btn btn-lg btn-outline-dark">Acheter des photos</button><br><br>';
+          }
+          elseif ($_SESSION['login']=true AND $_SESSION['type']=='photographe') {
+            echo '<button type="button" class="btn btn-lg btn-outline-dark">Publier des photos</button><br><br>';
+          } else {
+            echo '<h1>Vous n\'êtes pas connecté !</h1><br><br>';
+          }
+        ?>
+</div>
 
 
       <?php 
--- profil.php
+++ profil.php
@@ -7,16 +7,17 @@
   ?>
 	<div class="container">
     	<div class="jumbotron">
-
-          <?php echo "<img src=images/".$_SESSION['photo']." id='photo'  width=20% class='img-responsive float-right' >" ?>
       		<h1 class="display-4">Votre profil de <?php echo $_SESSION['type'] ?></h1>
+          <?php echo "<img src=".$_SESSION['photo']." id='photo'  width=12% class='img-responsive float-left' >" ?>
       		<?php echo '<p class="lead">Bonjour '.$_SESSION['nomUtilisateur'].' !</p>'?>
       		<hr class="my-4">
           <?php echo "<p class='lead'>Nom : ".$_SESSION['nomUtilisateur']."</p>" ?>
           <?php echo "<p class='lead'>Prénom : ".$_SESSION['prenomUtilisateur']."</p>" ?>
           <?php echo "<p class='lead'>Courriel : ".$_SESSION['emailUtilisateur']."</p>" ?>
           <?php echo "<p class='lead'>Vos crédit(s) : ".$_SESSION['credit']."</p>" ?>
+          <hr class="my-4">
     </div>
+  </div>
   <?php
     include ("include/piedpage.inc.php");
 ?>
