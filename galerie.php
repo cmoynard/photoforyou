e1d66245f3c3155b7bd4277c2f73fbf1d0eb8a82
cmoynard <cmoynard.etude@gmail.com>
2021-11-14 05:04:58 +0100

    BUG RESOLU ACHAT PHOTO OMG

--- galerie.php
+++ galerie.php
@@ -14,10 +14,11 @@
       <hr class="my-4">
     </div>
   </div>
-  <form action="achat.php" method="POST">
+  
   <div class="container">
   <div class="row">
     <div class="col-sm-3">
+    <form action="achat.php" method="POST">
       <div class="card">
         <?php 
           $idPhoto = 1;
@@ -40,8 +41,10 @@
           <button type="submit" value="acheter" name="acheter" class="btn btn-success">Acheter pour 45 crédits</button>
         </div>
       </div>
+    </form>
     </div>
     <div class="col-sm-3">
+    <form action="achat.php" method="POST">
       <div class="card">
       <?php
           $query = "SELECT * from photoforyou.galerie where idPhoto = $idPhoto;";
@@ -63,15 +66,20 @@
           <button type="submit" value="acheter" name="acheter" class="btn btn-success">Acheter pour 45 crédits</button>
         </div>
       </div>
+    </form>
     </div>
     <div class="col-sm-3">
+    <form action="achat.php" method="POST">
       <div class="card">
-        <?php
+      <?php
           $query = "SELECT * from photoforyou.galerie where idPhoto = $idPhoto;";
           $requete = $db->query($query);
           $result = $requete->fetch();
           $galPhoto = "images/galerie/".htmlentities($result['nomPhoto']);
-          echo "<img src='https://mdbootstrap.com/img/new/standard/nature/184.jpg' class='card-img-top' alt='...'/>";
+          ?> 
+          <input type="number" name="nbCredit" value=45 style="display: none;"/> 
+          <input type="hidden" name="idAchat" value="<?php echo $idPhoto; ?>" />
+          <?php echo "<img src=$galPhoto class='card-img-top' alt='...'/>";
           $idPhoto++;?>
           
         <div class="card-body">
@@ -83,15 +91,20 @@
           <button type="submit" value="acheter" name="acheter" class="btn btn-success">Acheter pour 45 crédits</button>
         </div>
       </div>
+    </form>
     </div>
     <div class="col-sm-3">
+    <form action="achat.php" method="POST">
       <div class="card">
-        <?php
+      <?php
           $query = "SELECT * from photoforyou.galerie where idPhoto = $idPhoto;";
           $requete = $db->query($query);
           $result = $requete->fetch();
           $galPhoto = "images/galerie/".htmlentities($result['nomPhoto']);
-          echo "<img src='https://mdbootstrap.com/img/new/standard/nature/184.jpg' class='card-img-top' alt='...'/>";
+          ?> 
+          <input type="number" name="nbCredit" value=45 style="display: none;"/> 
+          <input type="hidden" name="idAchat" value="<?php echo $idPhoto; ?>" />
+          <?php echo "<img src=$galPhoto class='card-img-top' alt='...'/>";
           $idPhoto++;?>
           
         <div class="card-body">
@@ -103,10 +116,10 @@
           <button type="submit" value="acheter" name="acheter" class="btn btn-success">Acheter pour 45 crédits</button>
         </div>
       </div>
+    </form>
     </div>
   </div>
   </div>
-  </form>
 
   <div class="container">
     <div class="jumbotron">
