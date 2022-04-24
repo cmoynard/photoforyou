7882dc5f6daa294113c2ae75af344a73792bab79
cmoynard <cmoynard.etude@gmail.com>
2022-04-24 23:06:25 +0200

    affichage photo swag OK

--- profil.php
+++ profil.php
@@ -17,19 +17,64 @@
           <?php echo "<p class='lead'>Vos crédit(s) : ".$_SESSION['credit']."</p>" ?>
           <hr class="my-4">
           <a href="rajcredit.php"><button type="button" class="btn btn-lg btn-outline-dark">Rajouter des crédits.</button></a>
-          <hr class="my-4">
-          <p class="lead">Photos acheté : </p>
-          <div class="row row-cols-6">
-            
           <?php
+            if ($_SESSION['type'] == 'photographe') {
+            
+              $query = "SELECT * from photoforyou.galerie where id_vendeur = ".$_SESSION['idUtilisateur'].";";
+              $requete = $db->query($query);
+              $result = $requete->fetchAll();
+              $photos_vendus = [];
+
+              if (!empty($result)) {
+                echo '<hr class="my-4">';
+                echo '<p class="lead">Photos en vente : </p>';
+                echo '<div class="row row-cols-6">';
+
+                foreach ($result as $ligne) {
+                  if ($ligne['id_acheteur'] == NULL) {
+                    echo '<div class="col">';
+                    echo "<img src='images/galerie/".htmlentities($ligne['nomPhoto'])."' id='photo' class='img-responsive float-left h-100 w-100' >" ;
+                    echo '</div>';
+                  } else {
+                    array_push($photos_vendus, $ligne);
+                  }
+                } echo '</div>';
+              }
+              if (!empty($photos_vendus)) {
+                
+                echo '<hr class="my-4">';
+                echo '<p class="lead">Photos vendus : </p>';
+                echo '<div class="row row-cols-6">';
+                
+                foreach ($photos_vendus as $ligne) {
+                  echo '<div class="col">';
+                  echo "<img src='images/galerie/".htmlentities($ligne['nomPhoto'])."' id='photo' class='img-responsive float-left h-100 w-100' >" ;
+                  echo '</div>';
+
+                } echo '</div>';
+              }
+            } else {
+
               $query = "SELECT * from photoforyou.galerie where id_acheteur = ".$_SESSION['idUtilisateur'].";";
               $requete = $db->query($query);
               $result = $requete->fetchAll();
-              foreach ($result as $ligne) {
-                echo '<div class="col">';
-                echo "<img src='images/galerie/".htmlentities($ligne['nomPhoto'])."' id='photo' class='img-responsive float-left h-100 w-100' >" ;
-                echo '</div>';
-            } ?>
+              
+              if (!empty($result)) {
+                echo '<hr class="my-4">';
+                echo '<p class="lead">Photos achetées : </p>';
+                echo '<div class="row row-cols-6">';
+
+                $query = "SELECT * from photoforyou.galerie where id_acheteur = ".$_SESSION['idUtilisateur'].";";
+                $requete = $db->query($query);
+                $result = $requete->fetchAll();
+                foreach ($result as $ligne) {
+                  echo '<div class="col">';
+                  echo "<img src='images/galerie/".htmlentities($ligne['nomPhoto'])."' id='photo' class='img-responsive float-left h-100 w-100' >" ;
+                  echo '</div>';
+                }
+              }  
+            }
+          ?>
           </div>
           <hr class='my-4'>
             
@@ -39,4 +84,4 @@
   </div>
   <?php
     include ("include/piedpage.inc.php");
-?>
+  ?>
